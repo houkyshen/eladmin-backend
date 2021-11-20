57b0ad5866aa7adddabb8ae638b52b1cea2ad02e
HoukyShen <786501105@qq.com>
2021-11-20 12:24:11 +0800

    1. 初始化项目

---
+++ src/main/java/com/jeff/EladminBackendApplication.java
@@ -0,0 +1,11 @@
+package com.jeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EladminBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EladminBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jeff/eladminbackend/EladminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.eladminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
