974366a12e4adbc4d9dbb62c8f07f20805b882bf
houkyshen <786501105@qq.com>
2022-02-24 20:11:13 +0800

    3.2 让错误提示消息正常显示

--- src/main/java/com/jeff/config/SecurityConfig.java
+++ src/main/java/com/jeff/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jeff.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,7 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/auth/login").permitAll()
-                .antMatchers("/auth/code").permitAll().anyRequest().authenticated()
+                .antMatchers("/auth/code").permitAll()
+                // 放行OPTIONS请求，放行了才能把status放到data里面
+                .antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
+                .anyRequest().authenticated()
         .and().csrf().disable();
     }
 }
